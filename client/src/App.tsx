95139b9d27fe789494b0dab4e3341cdb394405c6
sminha <sminha8590@naver.com>
2026-03-05 02:56:50 +0900

    init: TANSTACK QUERY 세팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,25 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './App.css';
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5,
+        gcTime: 1000 * 60 * 10,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: true,
+        retry: 1,
+      },
+    },
+  });
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <p className="font-bold text-red-300">안녕하세요</p>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
