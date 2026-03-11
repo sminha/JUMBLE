09fc47ed8ca917763df220f12d07081acd3afca7
sminha <sminha8590@naver.com>
2026-03-12 01:12:57 +0900

    init: 글로벌 스타일 설정

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,3 +1,40 @@
 export default function Home() {
-  return <p className="font-bold text-red-300">안녕하세요</p>;
+  return (
+    <>
+      <div className="bg-primary-1">primary-1</div>
+      <div className="bg-primary-3">primary-3</div>
+      <div className="bg-primary">primary</div>
+      <div className="bg-secondary-1">secondary-1</div>
+      <div className="bg-secondary-3">secondary-3</div>
+      <div className="bg-secondary-5">secondary-5</div>
+      <div className="bg-gray-1">gray-1</div>
+      <div className="bg-gray-2">gray-2</div>
+      <div className="bg-gray-3">gray-3</div>
+      <div className="bg-gray-4">gray-4</div>
+      <div className="bg-gray-5">gray-5</div>
+      <div className="bg-gray-6">gray-6</div>
+      <div className="bg-gray-7">gray-7</div>
+      <div className="bg-gray-8">gray-8</div>
+      <div className="bg-gray-9">gray-9</div>
+      <div className="bg-error">error</div>
+      <div className="bg-background">background</div>
+      <div className="bg-overlay">overlay</div>
+      <div className="bg-selected">selected</div>
+
+      <p className="title-28-b">title-28-b</p>
+      <p className="title-28-sb">title-28-sb</p>
+      <p className="title-24-sb">title-24-sb</p>
+      <p className="title-22-m">title-22-m</p>
+      <p className="title-20-m">title-20-m</p>
+      <p className="title-18-sb">title-18-sb</p>
+      <p className="title-16-sb">title-16-sb</p>
+      <p className="font-20-m">font-20-m</p>
+      <p className="font-18-sb">font-18-sb</p>
+      <p className="font-18-r">font-18-r</p>
+      <p className="font-16-sb">font-16-sb</p>
+      <p className="font-16-r">font-16-r</p>
+      <p className="font-14-r">font-14-r</p>
+      <p className="font-12-m">font-12-m</p>
+    </>
+  );
 }
