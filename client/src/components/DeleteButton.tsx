558d82e41bb5cbe0586e20c31c6d1da5e1cc7c6a
sminha <sminha8590@naver.com>
2026-03-29 23:41:54 +0900

    feat: Dropdown, DeleteButton 공통 컴포넌트 추가

---
+++ client/src/components/DeleteButton.tsx
@@ -0,0 +1,20 @@
+import { ButtonHTMLAttributes } from 'react';
+import deleteIcon from '@/assets/delete.png';
+import { cn } from '@/utils/cn';
+
+interface DeleteButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  onClick: () => void;
+}
+
+export default function DeleteButton({ onClick, className, ...props }: DeleteButtonProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={cn('h-[1.6rem] w-[1.6rem]', className)}
+      {...props}
+    >
+      <img src={deleteIcon} />
+    </button>
+  );
+}
