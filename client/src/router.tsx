dd1834732eb52e42b89ee6df3e091e8e6f03cda3
sminha <sminha8590@naver.com>
2026-04-10 17:31:10 +0900

    feat(*): ProtectedRoute, 인증 미들웨어 적용

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,11 +1,20 @@
-import { createBrowserRouter } from 'react-router';
+import { Outlet, Navigate, createBrowserRouter } from 'react-router';
 import Home from '@/pages/home/Home';
+import KakaoCallback from './pages/home/KakaoCallback';
 import PurchaseNew from '@/pages/purchases/new/PurchaseNew';
+import PurchaseList from './pages/purchases/list/PurchaseList';
 
 export const PATHS = {
   HOME: '/',
+  KAKAOCALLBACK: '/auth/kakao/callback',
   PURCHASENEW: '/purchases/new',
+  PURCHASELIST: '/purchases/list',
 } as const;
+
+function ProtectedRoute() {
+  const token = localStorage.getItem('accessToken');
+  return token ? <Outlet /> : <Navigate to={PATHS.HOME} replace />;
+}
 
 export const router = createBrowserRouter([
   {
@@ -13,7 +22,20 @@ export const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: PATHS.PURCHASENEW,
-    element: <PurchaseNew />,
+    path: PATHS.KAKAOCALLBACK,
+    element: <KakaoCallback />,
+  },
+  {
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: PATHS.PURCHASENEW,
+        element: <PurchaseNew />,
+      },
+      {
+        path: PATHS.PURCHASELIST,
+        element: <PurchaseList />,
+      },
+    ],
   },
 ]);
