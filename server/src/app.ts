3b6effc72e92ebdafb0cd4db4294cae409713b13
sminha <sminha8590@naver.com>
2026-04-27 22:13:30 +0900

    feat(server): cors 설정

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,7 +13,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://localhost:4000', 'https://jumble-client.vercel.app'],
+  }),
+);
+app.options('*', cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/v1/auth', authRouter);
