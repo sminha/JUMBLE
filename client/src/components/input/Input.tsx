1a8f24abc8e3470cebc9bf0a3dad5779b3fdd7ff
sminha <sminha8590@naver.com>
2026-04-30 17:32:45 +0900

    refactor(client): Input에 검색 버튼 옵션 추가

--- client/src/components/input/Input.tsx
+++ client/src/components/input/Input.tsx
@@ -1,11 +1,15 @@
 import { cn } from '@/utils/cn';
+import { Search } from 'lucide-react';
 import { Status, STATUS, STATUS_STYLE } from '@/constants/status';
+import { SearchProps } from '@/pages/purchases/list/types/search';
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface BaseInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   numeric?: boolean;
   status?: Status;
   errorMessage?: string;
 }
+
+type InputProps = BaseInputProps & SearchProps;
 
 const NUMERIC_ALLOWED_KEYS = [
   'Backspace',
@@ -23,9 +27,11 @@ export default function Input({
   numeric = false,
   status = STATUS.DEFAULT,
   errorMessage = '',
-  className,
+  hasSearchIcon = false,
+  onSearch,
   onChange,
   onKeyDown,
+  className,
   ...props
 }: InputProps) {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -55,6 +61,7 @@ export default function Input({
         className={cn(
           'font-14-r text-gray-6 placeholder:text-gray-4 rounded-[0.8rem] border bg-white p-[1.2rem]',
           disabled && 'bg-gray-1 cursor-not-allowed',
+          hasSearchIcon && 'pr-[3.8rem]',
           STATUS_STYLE[status],
           className,
         )}
@@ -63,6 +70,15 @@ export default function Input({
         onKeyDown={handleKeyDown}
         {...props}
       />
+      {hasSearchIcon === true && (
+        <button
+          onClick={onSearch}
+          aria-label="검색"
+          className="text-gray-5 absolute top-1/2 right-[1rem] shrink-0 -translate-y-1/2"
+        >
+          <Search size={20} />
+        </button>
+      )}
       {status === STATUS.ERROR && (
         <p className="font-12-r text-error absolute top-full left-[0.1rem] mt-[0.3rem]">
           {errorMessage}
