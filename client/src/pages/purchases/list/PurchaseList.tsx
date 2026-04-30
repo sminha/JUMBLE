c04ea6e81f2b9dc347735a132b0e6cbab243cb37
sminha <sminha8590@naver.com>
2026-04-30 13:05:21 +0900

    feat(client): PurchaseList 반응형 구현

--- client/src/pages/purchases/list/PurchaseList.tsx
+++ client/src/pages/purchases/list/PurchaseList.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import { Header } from '@/components';
-import FilterSection from './sections/FilterSection';
-import ResultSection from './sections/ResultSection';
 import { Draft, INITIAL_DRAFT } from '@jumble/shared';
+import { Header } from '@/components';
 import { useGetPurchases } from '@/pages/purchases/list/apis';
+import FilterSection from '@/pages/purchases/list/sections/FilterSection';
+import ResultSection from '@/pages/purchases/list/sections/ResultSection';
 
 export default function PurchaseList() {
   const [draft, setDraft] = useState<Draft>(INITIAL_DRAFT);
@@ -13,8 +13,10 @@ export default function PurchaseList() {
   return (
     <main>
       <Header />
-      <h1 className="title-18-m text-gray-9 px-[7.2rem] py-[3.2rem]">사입 내역 조회</h1>
-      <div className="flex flex-col gap-[3.2rem] px-[6.4rem] pb-[3.2rem]">
+      <h1 className="title-18-m text-gray-9 hidden px-[clamp(2rem,calc(11vw-5.4rem),6.4rem)] py-[2.4rem] sm:flex">
+        사입 내역 조회
+      </h1>
+      <div className="flex flex-col gap-[2rem] px-[2rem] px-[clamp(2rem,calc(11vw-5.4rem),6.4rem)] pt-[2rem] pb-[3.2rem] sm:pt-0">
         <FilterSection draft={draft} setDraft={setDraft} onSearch={() => setParams(draft)} />
         <ResultSection
           params={params}
